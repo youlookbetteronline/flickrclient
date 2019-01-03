24d225027585f04ee10a2c8c4909ae5f5447c493
youlookbetteronline <o.qinash@gmail.com>
2019-01-03 20:07:05 +0200

    finishtask

---
+++ app/src/main/java/com/example/gav/flickrclient/feed/FullsizePhotoActivity.java
@@ -0,0 +1,13 @@
+package com.example.gav.flickrclient.feed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullsizePhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullsize_photo);
+    }
+}
